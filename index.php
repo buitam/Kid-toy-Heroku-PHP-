40f71aca8ab3f9ba4423d1d6e0b97fcbf13914f2
Bui Tam <buitam2708@gmail.com>
2019-04-30 13:10:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ $sql = "SELECT id, name FROM label";
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["localhost"],
+    $db["ec2-54-221-236-144.compute-1.amazonaws.com"],
     $db["5432"],
-    $db["postgres"],
-    $db["pass"],
-    ltrim($db["postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu"], "/")
+    $db["wwdwlzfwmqeuxe"],
+    $db["a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a"],
+    ltrim($db["d7kjtop6ijkhiu"], "/")
 ));
 $stmt = $pdo->prepare($sql);
 //Thiết lập kiểu dữ liệu trả về
