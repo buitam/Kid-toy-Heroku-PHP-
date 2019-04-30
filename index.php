c2ce0b2812193d63e0fffd1ab5c8efb4dbfe2b7d
Bui Tam <buitam2708@gmail.com>
2019-04-30 13:40:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $y = 5;
 echo "<h2>" . $txt1 . "</h2>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo $x + $y;
-$db = parse_url(getenv("postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu"));
+define('ROOT_URL','postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu');
 $host = "ec2-54-221-236-144.compute-1.amazonaws.com";
 $user = "wwdwlzfwmqeuxe";
 $password = "a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a";
