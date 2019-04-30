2dfb6c57e0c290a2c626a166f51f09a423f785a6
Bui Tam <buitam2708@gmail.com>
2019-04-30 17:37:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
 <?php 
 $sql = "SELECT id, name FROM label";
-$db = parse_url(getenv("postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu"));
+$db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["ec2-54-221-236-144.compute-1.amazonaws.com"],
