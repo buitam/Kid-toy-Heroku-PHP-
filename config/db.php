308139738fe9626a5d172acd2199d182dda96a1a
Bui Tam <buitam2708@gmail.com>
2019-04-30 14:17:28 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,23 +1,11 @@
 <?php
-
-$host = "ec2-54-221-236-144.compute-1.amazonaws.com";
-$user = "wwdwlzfwmqeuxe";
-$password = "a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a";
-$dbname = "d7kjtop6ijkhiu";
-$port = "5432";
-
-try{
-  //Set DSN data source name
-    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
-
-
-  //create a pdo instance
-  $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-echo 'Connection failed: ' . $e->getMessage();
-}
+$db = parse_url(getenv("postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["ec2-54-221-236-144.compute-1.amazonaws.com"],
+    $db["5432"],
+    $db["wwdwlzfwmqeuxe"],
+    $db["a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a"],
+    ltrim($db["d7kjtop6ijkhiu"], "/")
+));
   ?>
