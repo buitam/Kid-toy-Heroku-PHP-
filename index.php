18b79fc99ec3c5b4d0ea2c9e04387f5135e0290b
Bui Tam <buitam2708@gmail.com>
2019-04-30 13:52:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 <?php
   //echo 'This is Index Page';
 
-  $sql = 'SELECT * FROM users';
+  $sql = 'SELECT * FROM label';
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
   $rowCount = $stmt->rowCount();
