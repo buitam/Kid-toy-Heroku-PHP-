ea6c10eeebf750f00bfc6990ac49c40aa3134d10
Bui Tam <buitam2708@gmail.com>
2019-04-30 14:24:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include ('config/db.php')?>
 <?php include ('config/config.php')?>
 
 <html>
@@ -29,9 +28,17 @@
       </div>
 </nav>
 
-<?php
-  //echo 'This is Index Page';
+<?php 
 $sql = "SELECT id, name FROM label";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["ec2-54-221-236-144.compute-1.amazonaws.com"],
+    $db["5432"],
+    $db["wwdwlzfwmqeuxe"],
+    $db["a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a"],
+    ltrim($db["postgres://wwdwlzfwmqeuxe:a9908f22ddf78c99c2278ddc7fe2942c1784dbb47b69d3d8a3492a02c0c9f56a@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d7kjtop6ijkhiu"], "/")
+));
 $stmt = $pdo->prepare($sql);
 //Thiết lập kiểu dữ liệu trả về
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
