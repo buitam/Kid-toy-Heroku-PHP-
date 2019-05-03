801b1e9b58491582c344cfc282c4f7e76b1fe4ba
Bui Tam <buitam2708@gmail.com>
2019-05-03 14:21:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,21 @@
-<?php include ('config/config.php')?>
+<!DOCTYPE html>
 <html>
-<head>
-  <title>Heroku Test</title>
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/index.css">
-</head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Heroku Test</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li>
-        <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
-      </li>
-      <li>
-        <a class="nav-link" href="<?php echo ROOT_URL; ?>about">About</a>
-      </li>
-      <li>
-        <a class="nav-link" href="<?php echo ROOT_URL; ?>contact">Contact Us</a>
-      </li>
-    </ul>
-      </div>
-</nav>
-<?php
- define('ROOT_URL','https://simpletam1122.herokuapp.com/');
-$host = "ec2-54-221-198-156.compute-1.amazonaws.com";
-$user = "zfpdnlhemvtirq";
-$password = "5a04911d431798731efce0fd4259e82e21bc3605c0c7091295c07cfe77872613";
-$dbname = "d9d290hur82qko";
-$port = "5432";
-try{
-  //Set DSN data source name
-    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
-  //create a pdo instance
-  $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-echo 'Connection failed: ' . $e->getMessage();
-}
-  //echo 'This is Index Page';
-  $sql = 'SELECT * FROM users';
-  $stmt = $pdo->prepare($sql);
+
+<h1>My first PHP page</h1>
+
+<?php 
+$sql = "SELECT id, name FROM label";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
 //Thiết lập kiểu dữ liệu trả về
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $stmt->execute();
@@ -55,3 +24,5 @@ foreach ($resultSet as $row) {
   echo $row['name'] . '\n';
 }
 ?>
+</body>
+</html>
