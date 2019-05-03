5051d83a6fd2eb33a39846ac5350b7e42639b6b9
Bui Tam <buitam2708@gmail.com>
2019-05-04 00:35:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -146,7 +146,32 @@ window.onclick = function(event) {
 					<li><a href="VindeFrance.php" class="hvr-underline-from-center"> 
 
 </a></li>
-					
+				<?php 
+$sql = "SELECT * FROM product";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+foreach ($resultSet as $row) {
+	echo $row['Productid'] . "<br/>";
+	echo $row['Catid'] . "<br/>";
+	echo $row['Productname'] . "<br/>";
+	echo $row['Price'] . "<br/>";
+	echo $row['Image'] . "<br/>";
+	echo $row['Description'] . "<br/>";
+	echo $row['Discount'] . "<br/>";
+}
+?>	
 					
 				</ul>
 
