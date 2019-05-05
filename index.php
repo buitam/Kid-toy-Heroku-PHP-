0f3fd8bbf6c43f8eabf2f12a34b5b05bb6bf8bee
Bui Tam <buitam2708@gmail.com>
2019-05-05 08:46:46 +0700

    hahha

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ window.onclick = function(event) {
   <form class="modal-content" action="Signup.php" method="POST">
     <div class="container">
       <h1>Sign Up</h1>
-      <p>Please fill in this form to create an account.</p>
+      <p>Please fills in this form to create an account.</p>
       <hr>
       <label for="username"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="user" required>
